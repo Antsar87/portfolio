e5816c9b9c19a9f70fbadaaaa84293931891642e
antsar <jomay1129@gmail.com>
2022-05-17 19:07:26 -0600

    first page finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
 const btn = document.querySelector('#btn-slide');
-const btnClose = document.querySelector(".btn-close")
+const btnClose = document.querySelector('.btn-close');
 const slide = document.querySelector('.slide-nav');
 const screen = document.querySelector('.overScreen');
 
 btn.addEventListener('click', () => {
   slide.classList.add('slide-navShow');
-  screen.classList.add("overScreenShow")
+  screen.classList.add('overScreenShow');
 });
 
-btnClose.addEventListener("click", () => {
-    slide.classList.remove("slide-navShow")
-    screen.classList.remove("overScreenShow")
-})
+btnClose.addEventListener('click', () => {
+  slide.classList.remove('slide-navShow');
+  screen.classList.remove('overScreenShow');
+});
+
+// navigation
+
+// gallery
+const btnMore = document.querySelector('#more');
+const card = document.querySelectorAll('.card');
+
+btnMore.addEventListener('click', (e) => {
+  card.forEach((e) => {
+    if (e.className.includes('card-none')) {
+      e.classList.toggle('card-active');
+    }
+  });
+
+  console.log(btnMore.textContent);
+
+  if (btnMore.textContent === 'SHOW MORE') {
+    btnMore.textContent = 'SHOW LESS';
+  } else {
+    btnMore.textContent = 'SHOW MORE';
+  }
+
+  btnMore.classList.toggle('btn--yellow');
+});
